d829d611a163964070684c916fe1aed491072d3e
Ty Austin <ty.austin1@gmail.com>
2025-10-23 22:08:07 -0400

    Initial commit

---
+++ ford-tdd/src/main/java/com/ford/ford_tdd/FordTddApplication.java
@@ -0,0 +1,13 @@
+package com.ford.ford_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FordTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FordTddApplication.class, args);
+	}
+
+}
