17dad1d93c9e4e920990c6ae45757d8eb6ff6ad7
TAUSTI20 <TAUSTI20@ford.com>
2025-10-23 22:06:20 -0400

    Initial commit

---
+++ src/test/java/com/ford/ford_tdd/FordTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ford.ford_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FordTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
